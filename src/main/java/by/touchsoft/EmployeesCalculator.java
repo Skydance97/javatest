3a88ae3bb563821b645a69163febd2ccb69bfbd0
Skydance97 <skydance97@gmail.com>
2018-08-08 21:10:20 +0300

    Initial commit

---
+++ src/main/java/by/touchsoft/EmployeesCalculator.java
@@ -0,0 +1,11 @@
+package by.touchsoft;
+
+/**
+ * @author Alexander Shukaylo
+ * @see
+ */
+public class EmployeesCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
